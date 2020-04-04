f6873a2a2255bb03e32c84e69278759e21ee9a71
矢光公法 <yako.kiminori@no1s.biz>
2020-04-04 20:53:02 +0900

    from mac
    practice HTML CSS JS

---
+++ test/test1/index.php
@@ -0,0 +1,24 @@
+<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+<script src="sample.js">
+<script>
+console.log("this ");
+</script>
+<link rel="stylesheet" href="sample.css">
+<div class="head">
+    <div class="head-text">Header</div>
+</div>
+<div class="content">
+    <div class="content-left">
+        <p>content-left</p>
+    </div>
+    <div class="content-center">
+        <p>content-center</p>
+    </div>
+    <div class="content-right">
+        <p>content-right</p>
+    </div>
+</div>
+<div class="foot">
+    <p>foot</p>
+</div>
+
---
+++ test/test1/sample.js
@@ -0,0 +1,8 @@
+$(function() {
+
+    $('#text').click(function() {
+        $('#h1').css('color', 'red');
+        $('#p').css('color', 'blue');
+        console.log("this is test");
+    });
+});
