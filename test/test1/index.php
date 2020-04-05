281f5958164a4bb21440acef1c4b98dea7fb594c
yamitsu <yako.kiminori@no1s.biz>
2020-04-05 20:19:32 +0900

    from win
    成田作成

--- test/test1/index.php
+++ test/test1/index.php
@@ -5,12 +5,35 @@ console.log("this ");
 </script>
 <link rel="stylesheet" href="sample.css">
 <div class="head">
-    <img class="narita-logo1" src="img/narita-logo2.png">
+    <div class="header-logo">
+      <img class="narita-logo2" src="img/narita-logo2.png">
+    </div>
     <div class="head-text">
-        <div>
-            <img class="wheelchair" src="img/wheelchair.png">
-            <p class="wheelchair">お手伝いの必要なお客様へ</p>
-        </div>
+        <ul class="head-ul1">
+            <li class="wheelchair">
+                <a class="top-btn" href="javascript:void(0)">お手伝いの必要なお客様へ</a>
+            </li>
+            <li class="question">
+                <a class="top-btn" href="javascript:void(0)">よくあるご質問</a>
+            </li>
+        </ul>
+        <ul class="head-ul2">
+            <li class="eng">
+                <a class="top-btn" href="javascript:void(0)">English</a>
+            </li>
+            <li class="kre">
+                <a class="top-btn" href="javascript:void(0)">ハングル</a>
+            </li>
+            <li class="chi1">
+                <a class="top-btn" href="javascript:void(0)">中国1</a>
+            </li>
+            <li class="chi2">
+                <a class="top-btn" href="javascript:void(0)">中国2</a>
+            </li> 
+            <li class="search">
+                <a class="top-btn" href="javascript:void(0)">検索</a>
+            </li>
+        </ul>
     </div>
 </div>
 <div class="content">
